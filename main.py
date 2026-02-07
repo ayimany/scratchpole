eb86ebea04ee81faa5f668259613425efdf3f7fd
ayimany <ayimany.dev@proton.me>
2026-02-07 17:25:20 -0600

    Implement simple, untested, min-zs process.

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import bpy
+import mathutils
+import os
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+PLATE_CENTER = (128, 128, 0)
+OUTPUT_PATH = sys.argv[-1]
+INPUT_PATH = sys.argv[-2]
+
+def clear_scene():
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.object.delete()
+
+def transform_object_to_center(obj):
+    bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='BOUNDS')
+
+    bbox_corners = [obj.matrix_world @ mathutils.Vector(corner) for corner in obj.bound_box]
+
+    min_z = min([v.z for v in bbox_corners])
+    obj.location.z = -min_z
+
+    bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='BOUNDS')
+    obj.location.x = PLATE_CENTER[0]
+    obj.location.y = PLATE_CENTER[1]
+
+    bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+def main():
+    clear_scene()
+    bpy.ops.import_mesh.stl(filepath=INPUT_PATH)
+    obj = bpy.context.selected_objects[0]
+    transform_object_to_center(obj)
+    bpy.ops.export_mesh.stl(filepath=OUTPUT_PATH)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
